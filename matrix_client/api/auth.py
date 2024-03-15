4e2ef93f5966e25a8232d8c012741cbc29a46656
bdvil <benjam.devillers@gmail.com>
2024-03-15 16:16:10 +0100

    API: add type annotation to auth GET. Remove URL

--- matrix_client/api/auth.py
+++ matrix_client/api/auth.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
+from typing import TypedDict
 
 from matrix_client.api.http_methods import GET
 
 
+class RegisterTokenValidityQuery(TypedDict):
+    token: str
+
+
 @dataclass
 class AuthEndpoints:
-    get_registration_token_validity = GET(
+    get_registration_token_validity: GET[RegisterTokenValidityQuery, None] = GET(
         "/_matrix/client/v1/register/m.login.registration_token/validity"
     )
--- matrix_client/api/http_methods.py
+++ matrix_client/api/http_methods.py
@@ -1,4 +1,3 @@
-from abc import ABC
 from collections.abc import Mapping
 from typing import Any
 from urllib import parse
@@ -18,23 +17,6 @@ def _urlencode(data: Mapping[str, Any] | None) -> str:
     return "?" + parse.urlencode(query)
 
 
-class URL:
-    def __init__(self, base_url: str):
-        self.base_url = base_url
-
-    def combine(self, other: "URL | str") -> "URL":
-        if isinstance(other, URL):
-            other = other.base_url
-
-        return URL(parse.urljoin(self.base_url, other))
-
-    def __repr__(self) -> str:
-        return self.base_url
-
-    def __truediv__(self, other: "URL | str") -> "URL":
-        return self.combine(other)
-
-
 class HTTPMethodNames(StrEnum):
     GET = "GET"
     POST = "POST"
@@ -42,13 +24,16 @@ class HTTPMethodNames(StrEnum):
     DELETE = "DELETE"
 
 
-class HTTPMethod[T: Mapping[str, Any], U](ABC):
+class HTTPMethod[T: Mapping[str, Any] | None, U]:
     method_name: HTTPMethodNames
 
     def __init__(self, url: str):
-        self.url = url
+        if isinstance(url, HTTPMethod):
+            self.url = url.url
+        else:
+            self.url = url
 
-    def parse(self, query_args: T | None = None, **kwargs: U) -> str:
+    def parse(self, query_args: T, **kwargs: U) -> str:
         url = self.url.format(**kwargs)
         return f"{url}{_urlencode(query_args)}"
 
@@ -56,28 +41,28 @@ class HTTPMethod[T: Mapping[str, Any], U](ABC):
         return self.url
 
 
-class GET[T: Mapping[str, Any], U](HTTPMethod[T, U]):
+class GET[T: Mapping[str, Any] | None, U](HTTPMethod[T, U]):
     method_name = HTTPMethodNames.GET
 
     def __repr__(self) -> str:
         return "GET " + super().__repr__()
 
 
-class POST[T: Mapping[str, Any], U](HTTPMethod[T, U]):
+class POST[T: Mapping[str, Any] | None, U](HTTPMethod[T, U]):
     method_name = HTTPMethodNames.POST
 
     def __repr__(self) -> str:
         return "POST " + super().__repr__()
 
 
-class PUT[T: Mapping[str, Any], U](HTTPMethod[T, U]):
+class PUT[T: Mapping[str, Any] | None, U](HTTPMethod[T, U]):
     method_name = HTTPMethodNames.PUT
 
     def __repr__(self) -> str:
         return "PUT " + super().__repr__()
 
 
-class DELETE[T: Mapping[str, Any], U](HTTPMethod[T, U]):
+class DELETE[T: Mapping[str, Any] | None, U](HTTPMethod[T, U]):
     method_name = HTTPMethodNames.DELETE
 
     def __repr__(self) -> str:
